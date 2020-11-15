618848f836d6af4f9494e8dc0012789bb022354f
Manish Patole <manish.patole@team.telstra.com>
2020-11-16 00:22:48 +1100

    Code clean up.

--- app/src/main/java/com/devmanishpatole/githubusers/base/BaseViewModel.kt
+++ app/src/main/java/com/devmanishpatole/githubusers/base/BaseViewModel.kt
@@ -10,11 +10,11 @@ import com.devmanishpatole.githubusers.util.Result
  */
 abstract class BaseViewModel : ViewModel() {
 
-    private val _messageStringId: MutableLiveData<Result<Int>> = MutableLiveData()
+    protected val _messageStringId: MutableLiveData<Result<Int>> = MutableLiveData()
     val messageStringId: LiveData<Result<Int>>
         get() = _messageStringId
 
-    private val _messageString: MutableLiveData<Result<String>> = MutableLiveData()
+    protected val _messageString: MutableLiveData<Result<String>> = MutableLiveData()
     val messageString: LiveData<Result<String>>
         get() = _messageString
 
